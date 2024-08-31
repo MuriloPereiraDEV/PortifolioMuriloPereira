9b2176de0b738a467ccaac327535aa4b0b3cb50c
Murilo Pereira <murilogalego10@gmail.com>
2024-08-31 14:34:38 -0300

    refact: Refatorando estrutura basica

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,16 +1,11 @@
 import { useTranslations } from "next-intl";
 
-import ButtonTheme from "@/components/theme/button-theme";
-import ButtonChangeLan from "@/components/language/button-change-language";
-
 export default function Home() {
   const t = useTranslations("Home");
 
   return (
     <main className="w-screen h-[86%] flex flex-col items-center justify-center">
       <h1>{t("title")}</h1>
-      <ButtonTheme />
-      <ButtonChangeLan />
     </main>
   );
 }
