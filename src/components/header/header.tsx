7ec676499ad02158e6bdec039b04380aafe2dc08
Murilo Pereira <murilogalego10@gmail.com>
2024-09-04 22:42:31 -0300

    feat: Termino do header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,13 +1,23 @@
 import ButtonChangeLan from "../language/button-change-language";
+import { GetTitle } from "../language/something-lan";
 import ButtonTheme from "../theme/button-theme";
 import Navbar from "./navbar";
 
 export default function Header() {
   return (
     <header className="w-screen h-[7%] flex items-center justify-end">
-      <Navbar />
-      <ButtonChangeLan />
-      <ButtonTheme />
+      <div className="w-[30%] h-[100%] flex items-center pl-32">
+        <h1 className="border-b-2 border-black dark:border-white">
+          <pre className="flex ">
+            Murilo Pereira | <GetTitle />
+          </pre>
+        </h1>
+      </div>
+      <div className="w-[70%] h-[100%] flex items-center justify-end pr-32">
+        <Navbar />
+        <ButtonChangeLan />
+        <ButtonTheme />
+      </div>
     </header>
   );
 }
--- src/components/language/button-change-language.tsx
+++ src/components/language/button-change-language.tsx
@@ -15,7 +15,7 @@ import {
 
 import { usePathname, useRouter } from "./navigation";
 import { useParams } from "next/navigation";
-import { useState, useTransition } from "react";
+import { useTransition } from "react";
 
 export default function ButtonChangeLan() {
   const t = useTranslations("Home");
@@ -35,7 +35,10 @@ export default function ButtonChangeLan() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="mx-2 bg-transparent">
-        <Badge className="px-1 py-2">
+        <Badge
+          className="p-2 px-1 py-2 hover:bg-accent hover:text-accent-foreground"
+          variant="outline"
+        >
           <Flag code={t("flag")} width={20} fallback={<span>Unknown</span>} />
           &darr;
         </Badge>
